f63308a338d863274432fa400f9c84057ed3fa94
familo23 <familo230506@gmail.com>
2026-09-23 14:09:17 +0700

    Bai tap 4.1

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblHoVaTen = new Label();
+            lblPassword = new Label();
+            txtHoVaTen = new TextBox();
+            txtPassword = new TextBox();
+            chkShowPassword = new CheckBox();
+            btnLogin = new Button();
+            btnExit = new Button();
+            SuspendLayout();
+            // 
+            // lblHoVaTen
+            // 
+            lblHoVaTen.AutoSize = true;
+            lblHoVaTen.Location = new Point(89, 68);
+            lblHoVaTen.Name = "lblHoVaTen";
+            lblHoVaTen.Size = new Size(82, 20);
+            lblHoVaTen.TabIndex = 0;
+            lblHoVaTen.Text = "Ho va Ten: ";
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(89, 120);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(73, 20);
+            lblPassword.TabIndex = 0;
+            lblPassword.Text = "Password:";
+            // 
+            // txtHoVaTen
+            // 
+            txtHoVaTen.Location = new Point(195, 65);
+            txtHoVaTen.Name = "txtHoVaTen";
+            txtHoVaTen.Size = new Size(370, 27);
+            txtHoVaTen.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(195, 113);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(370, 27);
+            txtPassword.TabIndex = 1;
+            // 
+            // chkShowPassword
+            // 
+            chkShowPassword.AutoSize = true;
+            chkShowPassword.Location = new Point(89, 178);
+            chkShowPassword.Name = "chkShowPassword";
+            chkShowPassword.Size = new Size(132, 24);
+            chkShowPassword.TabIndex = 2;
+            chkShowPassword.Text = "Show Password";
+            chkShowPassword.UseVisualStyleBackColor = true;
+            chkShowPassword.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(195, 229);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 29);
+            btnLogin.TabIndex = 3;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(328, 229);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(94, 29);
+            btnExit.TabIndex = 4;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(627, 303);
+            Controls.Add(btnExit);
+            Controls.Add(btnLogin);
+            Controls.Add(chkShowPassword);
+            Controls.Add(txtPassword);
+            Controls.Add(txtHoVaTen);
+            Controls.Add(lblPassword);
+            Controls.Add(lblHoVaTen);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblHoVaTen;
+        private Label lblPassword;
+        private TextBox txtHoVaTen;
+        private TextBox txtPassword;
+        private CheckBox chkShowPassword;
+        private Button btnLogin;
+        private Button btnExit;
+    }
+}
