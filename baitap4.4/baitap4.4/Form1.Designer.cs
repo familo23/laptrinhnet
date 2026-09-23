035907e07c6fe2bc38e50ebd420e53e1d1b2ca38
familo23 <familo230506@gmail.com>
2026-09-23 16:29:13 +0700

    baitap4.4

---
+++ baitap4.4/baitap4.4/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace baitap4._4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lstMenu = new System.Windows.Forms.ListBox();
+            this.lstSelected = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstMenu
+            // 
+            this.lstMenu.FormattingEnabled = true;
+            this.lstMenu.ItemHeight = 15;
+            this.lstMenu.Items.AddRange(new object[] {
+            "Hamburger - 50k",
+            "Pizza - 120k",
+            "Gà Rán - 35k",
+            "Pepsi - 15k"});
+            this.lstMenu.Location = new System.Drawing.Point(12, 12);
+            this.lstMenu.Name = "lstMenu";
+            this.lstMenu.Size = new System.Drawing.Size(260, 349);
+            this.lstMenu.TabIndex = 0;
+            // 
+            // lstSelected
+            // 
+            this.lstSelected.FormattingEnabled = true;
+            this.lstSelected.ItemHeight = 15;
+            this.lstSelected.Location = new System.Drawing.Point(428, 12);
+            this.lstSelected.Name = "lstSelected";
+            this.lstSelected.Size = new System.Drawing.Size(260, 349);
+            this.lstSelected.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(295, 120);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 35);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = ">";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(295, 170);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 35);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "<";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTotal.Location = new System.Drawing.Point(12, 380);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(72, 19);
+            this.lblTotal.TabIndex = 4;
+            this.lblTotal.Text = "Total: 0k";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 420);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstSelected);
+            this.Controls.Add(this.lstMenu);
+            this.Name = "Form1";
+            this.Text = "Menu Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstMenu;
+        private System.Windows.Forms.ListBox lstSelected;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
---
+++ baitap4.4/baitap4.4/Form1.cs
@@ -0,0 +1,10 @@
+namespace baitap4._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
